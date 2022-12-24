447642fc70731fc53bfd03af329223a059ac9752
Darak <84322513+luanwiz0@users.noreply.github.com>
2022-12-24 15:08:11 +0900

    Update Student20200962.py

--- HW4/Student20200962.py
+++ HW4/Student20200962.py
@@ -30,7 +30,7 @@ def classify0(inX, dataSet, labels, k):
 	classCount = {}
 	for i in range(k):
 		voteIlabel = labels[sortedDistIndicies[i]]
-		classCount[voteIlabel] = classCount.get(voteIlabel, 0)
+		classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
 	sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)
 	return sortedClassCount[0][0]
 
